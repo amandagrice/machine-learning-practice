f7b3d52698016d31f64d85f5a948b8d6a698d95b
Amanda Grice <algrice@bu.edu>
2020-05-26 22:45:16 -0400

    add start of dogs vs cats py script

---
+++ Dogs vs Cats/dogs_vs_cats.py
@@ -0,0 +1,213 @@
+import os, shutil
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+import sys
+from PIL import Image
+sys.modules['Image'] = Image
+
+'''
+# The path to the directory where the original
+# dataset was uncompressed
+original_dataset_dir = os.path.join('dogs-vs-cats', 'train')
+
+# The directory where we will
+# store our smaller dataset
+base_dir = 'cats_and_dogs_small'
+os.mkdir(base_dir)
+
+# Directories for our training,
+# validation and test splits
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+os.mkdir(train_dogs_dir)
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+os.mkdir(validation_cats_dir)
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+os.mkdir(validation_dogs_dir)
+
+# Directory with our validation cat pictures
+test_cats_dir = os.path.join(test_dir, 'cats')
+os.mkdir(test_cats_dir)
+
+# Directory with our validation dog pictures
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+os.mkdir(test_dogs_dir)
+
+# Copy first 1000 cat images to train_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 cat images to validation_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 cat images to test_cats_dir
+fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_cats_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy first 1000 dog images to train_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 dog images to validation_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(validation_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+# Copy next 500 dog images to test_dogs_dir
+fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(test_dogs_dir, fname)
+    shutil.copyfile(src, dst)
+
+print('total training cat images:', len(os.listdir(train_cats_dir)))            # 1000
+print('total training dog images:', len(os.listdir(train_dogs_dir)))            # 1000
+print('total validation cat images:', len(os.listdir(validation_cats_dir)))     # 500
+print('total validation dog images:', len(os.listdir(validation_dogs_dir)))     # 500
+print('total test cat images:', len(os.listdir(test_cats_dir)))                 # 500
+print('total test dog images:', len(os.listdir(test_dogs_dir)))                 # 500
+
+'''
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+'''
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #   
+=================================================================
+conv2d_1 (Conv2D)            (None, 148, 148, 32)      896       
+_________________________________________________________________
+max_pooling2d_1 (MaxPooling2 (None, 74, 74, 32)        0         
+_________________________________________________________________
+conv2d_2 (Conv2D)            (None, 72, 72, 64)        18496     
+_________________________________________________________________
+max_pooling2d_2 (MaxPooling2 (None, 36, 36, 64)        0         
+_________________________________________________________________
+conv2d_3 (Conv2D)            (None, 34, 34, 128)       73856     
+_________________________________________________________________
+max_pooling2d_3 (MaxPooling2 (None, 17, 17, 128)       0         
+_________________________________________________________________
+conv2d_4 (Conv2D)            (None, 15, 15, 128)       147584    
+_________________________________________________________________
+max_pooling2d_4 (MaxPooling2 (None, 7, 7, 128)         0         
+_________________________________________________________________
+flatten_1 (Flatten)          (None, 6272)              0         
+_________________________________________________________________
+dense_1 (Dense)              (None, 512)               3211776   
+_________________________________________________________________
+dense_2 (Dense)              (None, 1)                 513       
+=================================================================
+Total params: 3,453,121
+Trainable params: 3,453,121
+Non-trainable params: 0
+
+'''
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+'''
+Data needs to be formatted appropriately. 
+1. read images
+2. decode JPEG content to RGB grid of pixels
+3. convert these to floating point tensors
+4. rescale the pixel values from 0-255 to between 0 and 1
+'''
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_dir = os.path.join('cats_and_dogs_small', 'train')
+validation_dir = os.path.join('cats_and_dogs_small', 'validation')
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=30,
+      validation_data=validation_generator,
+      validation_steps=50)
+
+
+model.save('cats_and_dogs_small_1.h5')
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
